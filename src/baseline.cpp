95c3a0b86a44d78ca810c1227cac1694a0b218ab
Khaled Abdelaal <khaled.3ttia@gmail.com>
2021-07-07 16:39:05 +0000

    cleaner printing

--- src/baseline.cpp
+++ src/baseline.cpp
@@ -22,7 +22,16 @@ void generateMatrix(int nrows, int ncols, double nnzRate, double *&vals){
 
 }
 
-
+void printVector(double* ptr, int start, int end){
+	std::cout << "{\t";
+	for (int i = start; i < end; i++){
+		if (i == end-1){
+			std::cout << ptr[i] << "\t}" << std::endl;
+		}else{
+			std::cout << ptr[i] << "\t";
+		}
+	}
+}
 
 constexpr int NROWS = 1;
 constexpr int NCOLS = 10;
@@ -35,29 +44,13 @@ int main(){
     double* c = new double[NROWS * NCOLS];
 
     generateMatrix(NROWS, NCOLS, 0.7, a);
-
-    std::cout << "A: {" ;
-    for (int i = 0 ; i < NROWS * NCOLS ; i++){
-	if (i == NROWS * NCOLS - 1){
-		std::cout << a[i] << "}" << std::endl;
-	}else{
-		std::cout << a[i] << " "; 
-	}
-
-    }
+    std::cout << "A: " ;
+    printVector(a, 0, NROWS*NCOLS);
+    
 
     generateMatrix(NROWS, NCOLS, 0.2, b);
-
-    std::cout << "B: {" ;
-    for (int i = 0 ; i < NROWS * NCOLS ; i++){
-	if (i == NROWS * NCOLS - 1){
-		std::cout << b[i] << "}" << std::endl;
-	}else{
-		std::cout << b[i] << " "; 
-	}
-
-    }
-
+    std::cout << "B: " ;
+    printVector(b, 0, NROWS*NCOLS);
 
 #pragma omp target teams distribute parallel for map (to:a[0:NROWS*NCOLS], b[0:NROWS*NCOLS]) map(from:c[0:NROWS*NCOLS]) 
     for (int i = 0; i < NROWS * NCOLS; i++){
@@ -66,15 +59,7 @@ int main(){
     }
 
 
-    std::cout << "C: {" ;
-    for (int i = 0 ; i < NROWS * NCOLS ; i++){
-	if (i == NROWS * NCOLS - 1){
-		std::cout << c[i] << "}" << std::endl;
-	}else{
-		std::cout << c[i] << " "; 
-	}
-
-    }
-
+    std::cout << "C: " ;
+    printVector(c, 0, NROWS*NCOLS);
 
 }
