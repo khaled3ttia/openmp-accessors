71aa9f496a4d9b7a1a374bf84cec6bf2725426fa
Khaled Abdelaal <khaled.3ttia@gmail.com>
2021-07-07 16:15:46 +0000

    baseline using normal omp target
    baseline using regular omp target directive

--- src/baseline.cpp
+++ src/baseline.cpp
@@ -22,8 +22,10 @@ void generateMatrix(int nrows, int ncols, double nnzRate, double *&vals){
 
 }
 
-constexpr int NROWS = 1000;
-constexpr int NCOLS = 1000;
+
+
+constexpr int NROWS = 1;
+constexpr int NCOLS = 10;
 
 int main(){
    
@@ -33,11 +35,45 @@ int main(){
     double* c = new double[NROWS * NCOLS];
 
     generateMatrix(NROWS, NCOLS, 0.7, a);
+
+    std::cout << "A: {" ;
+    for (int i = 0 ; i < NROWS * NCOLS ; i++){
+	if (i == NROWS * NCOLS - 1){
+		std::cout << a[i] << "}" << std::endl;
+	}else{
+		std::cout << a[i] << " "; 
+	}
+
+    }
+
     generateMatrix(NROWS, NCOLS, 0.2, b);
 
+    std::cout << "B: {" ;
+    for (int i = 0 ; i < NROWS * NCOLS ; i++){
+	if (i == NROWS * NCOLS - 1){
+		std::cout << b[i] << "}" << std::endl;
+	}else{
+		std::cout << b[i] << " "; 
+	}
+
+    }
+
+
+#pragma omp target teams distribute parallel for map (to:a[0:NROWS*NCOLS], b[0:NROWS*NCOLS]) map(from:c[0:NROWS*NCOLS]) 
     for (int i = 0; i < NROWS * NCOLS; i++){
     
         c[i] = a[i] + b[i];
+    }
+
+
+    std::cout << "C: {" ;
+    for (int i = 0 ; i < NROWS * NCOLS ; i++){
+	if (i == NROWS * NCOLS - 1){
+		std::cout << c[i] << "}" << std::endl;
+	}else{
+		std::cout << c[i] << " "; 
+	}
+
     }
 
 
