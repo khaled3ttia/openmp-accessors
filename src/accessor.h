4541195b4e6338f363ab74815b856c6086c5221c
Khaled Abdelaal <khaled.3ttia@gmail.com>
2021-07-21 19:40:02 +0000

    merging accessor.h and accessor_impl.h in one file

--- src/accessor.h
+++ src/accessor.h
@@ -6,7 +6,7 @@ template <access_mode A, typename T>
 class Accessor_Base {
 protected:
     // Constructor to be used only by child classes
-    Accessor_Base(T* data, size_t len);
+    Accessor_Base(T* data, size_t len):_data(data), _len(len) {};
 public:
     // _data and _len need to be public to be used in declare mapper
     // pointer to the data
@@ -19,20 +19,20 @@ public:
 template <access_mode A, typename T>
 class Accessor: public Accessor_Base<A, T>{
 public:
-    Accessor(T*, size_t);
+    Accessor(T* data, size_t len): Accessor_Base<A, T>(data, len){ };
     // operator returns a reference
     // To allow data maniuplation in WRITE/READ_WRITE modes
-    T& operator[](const int) const;
+    T& operator[](const int idx) const { return this->_data[idx]; };
 };
 
 // Child Accessor 2: access_mode specialized for READ
 template <typename T>
 class Accessor<READ, T>: public Accessor_Base<READ, T>{
 public:
-    Accessor(T*, size_t);
+    Accessor(T* data, size_t len): Accessor_Base<READ, T>(data, len) {};
 
     // operator returns a const reference to prevent modification
-    const T& operator[](const int) const;
+    const T& operator[](const int idx) const { return this->_data[idx]; };
 };
 
 // different mappers based on access mode
@@ -49,5 +49,3 @@ public:
 #pragma omp declare mapper(Accessor<READ_WRITE, int> a) map(tofrom: a._data[0:a._len])
 
 
-
-#include "accessor_impl.h"
